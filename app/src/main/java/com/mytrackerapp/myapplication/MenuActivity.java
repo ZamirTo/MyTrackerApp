9bf8a1b3cfaf93a452a1f7059a0803f50a256b27
Tomer <toemrza@gmail.com>
2016-12-13 13:33:58 +0200

    change code for GPS button, add listener

--- app/src/main/java/com/mytrackerapp/myapplication/MenuActivity.java
+++ app/src/main/java/com/mytrackerapp/myapplication/MenuActivity.java
@@ -5,6 +5,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Handler;
+import android.os.Looper;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -14,23 +15,69 @@ import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static android.content.Context.LOCATION_SERVICE;
+
 import com.google.zxing.Result;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class MenuActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
 
+    private Button gpsBtn;
+    private LocationManager locationManager;
+    private LocationListener listener;
     private ZXingScannerView mScannerView;
-    final private int REQUEST_CAMERA_PERMISSIONS = 1;
+    final private int REQUEST_GPS_PERMISSIONS = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        gpsBtn = (Button) findViewById(R.id.gpsBtn);
 
-        ActivityCompat.requestPermissions(MenuActivity.this,
-                new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSIONS);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Intent intentBundle = new Intent(MenuActivity.this,MapActivity.class);
+                Bundle bundle = new Bundle();
+                String[] cords = {location.getLongitude()+"",location.getLatitude()+""};
+                bundle.putStringArray("cords", cords);
+                intentBundle.putExtras(bundle);
+                startActivity(intentBundle);
+            }
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
 
+            }
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+            @Override
+            public void onProviderDisabled(String s) {
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
+        configure_button();
     }
 
     @Override
@@ -45,20 +92,16 @@ public class MenuActivity extends AppCompatActivity implements ZXingScannerView.
     @Override
     public void onRequestPermissionsResult(int requestCode ,String[] permissions, int[] grantResults) {
         switch(requestCode){
-            case REQUEST_CAMERA_PERMISSIONS :{
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Log.e("Permission", "Camera permission granted");
-                } else {
-                    Log.e("Permission", "Camera permission denayied");
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
+            case REQUEST_GPS_PERMISSIONS: {
+                configure_button();
+                break;
+            }
+            default: {
+                break;
             }
         }
     }
+
 
     public void QrScanner(View view){
         mScannerView = new ZXingScannerView(this);
@@ -66,6 +109,26 @@ public class MenuActivity extends AppCompatActivity implements ZXingScannerView.
         mScannerView.setResultHandler(this);
         mScannerView.startCamera();
     }
+
+    void configure_button(){
+        // first check for permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.CAMERA,Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
+                        ,REQUEST_GPS_PERMISSIONS);
+            }
+            return;
+        }
+        // this code won't execute IF permissions are not allowed, because in the line above there is return statement.
+        gpsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //noinspection MissingPermission
+                locationManager.requestSingleUpdate("gps", listener, null);
+            }
+        });
+    }
+
 
     public void getFriends(View v){
         startActivity(new Intent(this,FriendsListActivity.class));
