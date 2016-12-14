cce16b41948cd8201b146ee7018e59de4148b1e5
Tomer <toemrza@gmail.com>
2016-12-14 09:42:44 +0200

    saving users cordinates

--- app/src/main/java/com/mytrackerapp/myapplication/newUserModel.java
+++ app/src/main/java/com/mytrackerapp/myapplication/newUserModel.java
@@ -3,6 +3,7 @@ package com.mytrackerapp.myapplication;
 public class newUserModel{
     private String name;
     private boolean checked = false;
+    private String cordinates;
 
     public newUserModel(){}
     public newUserModel(String _name){
@@ -12,20 +13,31 @@ public class newUserModel{
         this.name = name;
         this.checked = check;
     }
+    public newUserModel(String name, String cordinates, boolean check){
+        this.name = name;
+        this.checked = check;
+        this.cordinates = cordinates;
+    }
     public String getName() {
         return name;
     }
     public void setName(String name) {
         this.name = name;
     }
-    public boolean isChecked() {
-        return checked;
-    }
     public void setChecked(boolean checked) {
         this.checked = checked;
     }
+    public void setCordinates(String _cordinates){
+        this.cordinates = _cordinates;
+    }
+    public boolean isChecked() {
+        return checked;
+    }
     public String toString() {
         return name ;
+    }
+    public String getCordinates(){
+        return this.cordinates;
     }
     public void toggleChecked() {
         checked = !checked ;
